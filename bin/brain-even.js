9950b416583e38ec65d9a03b03bf53c3b80d7edf
Timur Davlet <timazavr@mail.ru>
2020-07-28 15:30:33 +0300

    доработка игры

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import * as users from '../src/run.js';
+
+users.run();
---
+++ src/run.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+export const getRandomInt = (max = 100) => Math.floor(Math.random() * Math.floor(max));
+
+export const isEven = (random) => (random % 2 === 0);
+
+export const userAnswer = (answer) => {
+  if (answer === 'yes') {
+    return true;
+  } if (answer === 'no') {
+    return false;
+  }
+  return null;
+};
+
+// eslint-disable-next-line consistent-return
+export const run = () => {
+  console.log('Welcome to the Brain Games!');
+
+  // eslint-disable-next-line no-shadow
+  const printName = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${printName}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const random = getRandomInt();
+    console.log(`Question: ${random}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (isEven(random) === userAnswer(answer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${answer === 'no' ? 'yes' : 'no'}.`);
+      return console.log(`Let's try again, ${printName}`);
+    }
+  }
+  console.log(`Congratulations, ${printName}`);
+};
